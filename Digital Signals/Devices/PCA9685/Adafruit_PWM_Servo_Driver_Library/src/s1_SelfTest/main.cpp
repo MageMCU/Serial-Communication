b9b9c9afa8fdac209e3489744d84e6cfa004a2e7
Jesse Carpenter <carpenterhesse@gmail.com>
2023-05-20 01:57:16 -0600

    Update main.cpp

--- Digital Signals/Devices/PCA9685/Adafruit_PWM_Servo_Driver_Library/src/s1_SelfTest/main.cpp
+++ Digital Signals/Devices/PCA9685/Adafruit_PWM_Servo_Driver_Library/src/s1_SelfTest/main.cpp
@@ -1,4 +1,22 @@
-
+//
+// Carpenter Software
+// File: Class BusI2C.h
+//
+// Purpose: Public Github Account - MageMCU
+// Repository:  
+// Folder:
+//
+// Author: Jesse Carpenter (carpentersoftware.com)
+// Email:carpenterhesse@gmail.com
+//
+// Testing Platform:
+//  * MCU:Atmega328P
+//  * Editor: VSCode
+//  * VSCode Extension: Microsoft C/C++ IntelliSense, debugging, and code browsing.
+//  * VSCode Extension:PlatformIO
+//
+// MIT LICENSE
+//
 #include "TestSetup.h"
 
 void setup()
